0f16573a02c1c986f8adf24853e189beacc51866
Santiago Flores Kanter <sfloresk@cisco.com>
2017-05-19 08:04:06 -0600

    Fix bugs

--- web_start.sh
+++ web_start.sh
@@ -2,7 +2,7 @@
 
 # creates passwd file to be able to ssh and scp
 
-export USER_ID=$(id -u) 
+export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 envsubst < /root/passwd.template > /usr/src/app/web_ui/nso_data/passwd
 export LD_PRELOAD=libnss_wrapper.so
