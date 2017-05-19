286b93a8cb0ddf5ae945ead4daa8a2cae0d14e64
Santiago Flores Kanter <sfloresk@cisco.com>
2017-05-19 07:52:43 -0600

    Fix bugs

--- web_start.sh
+++ web_start.sh
@@ -6,14 +6,14 @@ export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 envsubst < /root/passwd.template > /usr/src/app/web_ui/nso_data/passwd
 export LD_PRELOAD=libnss_wrapper.so
-export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_PASSWD=/usr/src/app/web_ui/nso_data/passwd
 export NSS_WRAPPER_GROUP=/etc/group
 
 # Set up any db change
-python manage.py makemigrations
+python /usr/src/app/manage.py makemigrations
 
 # Updates/Creates Database
-python manage.py migrate
+python /usr/src/app/manage.py migrate
 
 # Starts server
 python /usr/src/app/manage.py runserver 0.0.0.0:8025
